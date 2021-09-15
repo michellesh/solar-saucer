c7942fbf56540e6f1ebfdbd6cd0863ca1f1c4695
Michelle Sharer <michellesharer@gmail.com>
2021-09-15 13:46:12 -0600

    Implement sender for all actions except cycle color mode

--- libraries/solar_saucer_shared/solar_saucer_shared.h
+++ libraries/solar_saucer_shared/solar_saucer_shared.h
@@ -1,8 +1,35 @@
-#define ACTION_STROBE_ON   0
-#define ACTION_STROBE_OFF  1
+// Slider Actions
+#define ACTION_SET_BRIGHTNESS    0
+#define ACTION_SET_COLOR_LEFT    1
+#define ACTION_SET_COLOR_RIGHT   2
+#define ACTION_SPEED             3
+
+// Button Actions
+#define ACTION_CYCLE_COLOR_MODE  4
+#define ACTION_SET_BACKGROUND    5
+#define ACTION_STROBE_OFF        6
+#define ACTION_STROBE_ON         7
+
+// Auto Actions
+#define ACTION_SET_PALETTE       8
+
+// Visualizations
+#define VIZ_DEFAULT  0
+#define VIZ_TWINKLE  1
+#define VIZ_EXPLODE  2
+#define VIZ_SPIN     3
 
 typedef struct msg {
   uint8_t action;
-  int value1;
-  int value2;
+  int value;
 } msg;
+
+auto scale(float domainStart, float domainEnd, float rangeStart, float rangeEnd, bool clamp = false) {
+  return [=](float value) {
+    float percentage = (value - domainStart) / (domainEnd - domainStart);
+    float newValue = rangeStart + (rangeEnd - rangeStart) * percentage;
+    return clamp && newValue < min(rangeStart, rangeEnd)   ? min(rangeStart, rangeEnd)
+           : clamp && newValue > max(rangeStart, rangeEnd) ? max(rangeStart, rangeEnd)
+                                                           : newValue;
+  };
+}
--- sender/sender.ino
+++ sender/sender.ino
@@ -26,10 +26,37 @@ struct Slider {
   int prev;
 };
 
-int sliderIndex = -1;
+struct Timer {
+  unsigned long totalCycleTime;
+  unsigned long lastCycleTime;
+  void reset() {
+    lastCycleTime = millis();
+  };
+  bool complete() {
+    return (millis() - lastCycleTime) > totalCycleTime;
+  };
+};
 
-msg strobeOn = {ACTION_STROBE_ON};
+int backgrounds[] = {VIZ_TWINKLE, VIZ_EXPLODE, VIZ_SPIN};
+int activePalette = 0;
+int numPalettes = 9;
+int sliderIndex = -1;
+unsigned long backgroundCycleTime = 1000 * 60 * 4; // 4 minutes
+
+// Slider Actions
+msg brightness = {ACTION_SET_BRIGHTNESS};
+msg colorLeft = {ACTION_SET_COLOR_LEFT};
+msg colorRight = {ACTION_SET_COLOR_RIGHT};
+msg speed = {ACTION_SPEED};
+
+// Button Actions
+msg background = {ACTION_SET_BACKGROUND, VIZ_DEFAULT};
+msg cycleColorMode = {ACTION_CYCLE_COLOR_MODE};
 msg strobeOff = {ACTION_STROBE_OFF};
+msg strobeOn = {ACTION_STROBE_ON};
+
+// Auto Actions
+msg palette = {ACTION_SET_PALETTE};
 
 Button redButton = {RED_BUTTON, false};
 Button blueButton = {BLUE_BUTTON, false};
@@ -41,6 +68,12 @@ Slider slider1 = {SLIDER_1};
 Slider slider2 = {SLIDER_2};
 Slider slider3 = {SLIDER_3};
 Slider slider4 = {SLIDER_4};
+
+Timer backgroundTimer = {backgroundCycleTime, 0};
+
+auto sliderToBrightness = scale(1000, 50, 0, 255, true);
+auto sliderToHue = scale(900, 50, 255, 0, true);
+auto sliderToSpeed = scale(1000, 0, 1, 10, true);
 
 void setup() {
   Serial.begin(115200);
@@ -84,7 +117,36 @@ bool sliderValueChanged(Slider slider) {
   return slider.value < (slider.prev - BUFFER) || slider.value > (slider.prev + BUFFER);
 }
 
+void setBackground(int viz) {
+  background.value = background.value == viz ? VIZ_DEFAULT : viz;
+}
+
+void cycleBackground() {
+  int numBackgrounds = sizeof(backgrounds) / sizeof(backgrounds[0]);
+  int currentIndex = 0;
+  for (int i = 0; i < numBackgrounds; i++) {
+    if (backgrounds[i] == background.value) {
+      currentIndex = i;
+    }
+  }
+  int newIndex = currentIndex < numBackgrounds - 1 ? currentIndex + 1 : 0;
+  background.value = backgrounds[newIndex];
+}
+
 void loop() {
+  // Every N seconds, cycle through the active background viz
+  if (backgroundTimer.complete()) {
+    cycleBackground();
+    send(background);
+    backgroundTimer.reset();
+  }
+
+  EVERY_N_SECONDS(10) {
+    activePalette = activePalette == numPalettes - 1 ? 0 : activePalette + 1;
+    palette.value = activePalette;
+    send(palette);
+  }
+
   EVERY_N_MILLISECONDS(100) {
     if (sliderIndex == -1) {
       digitalWrite(slider1.pin, HIGH);
@@ -109,42 +171,34 @@ void loop() {
   }
 
   if (sliderValueChanged(slider1)) {
-    Serial.print("Slider 1 changed ");
-    Serial.println(slider1.value);
-
-    // TODO
-    //send();
-
+    brightness.value = sliderToBrightness(slider1.value);
+    Serial.print("BRIGHTNESS changed: ");
+    Serial.println(brightness.value);
+    send(brightness);
     slider1.prev = slider1.value;
   }
 
   if (sliderValueChanged(slider2)) {
-    Serial.print("Slider 2 changed ");
-    Serial.println(slider2.value);
-
-    // TODO
-    //send();
-
+    colorLeft.value = sliderToHue(slider2.value);
+    Serial.print("COLOR LEFT changed: ");
+    Serial.println(colorLeft.value);
+    send(colorLeft);
     slider2.prev = slider2.value;
   }
 
   if (sliderValueChanged(slider3)) {
-    Serial.print("Slider 3 changed ");
-    Serial.println(slider3.value);
-
-    // TODO
-    //send();
-
+    colorRight.value = sliderToHue(slider3.value);
+    Serial.print("COLOR RIGHT changed: ");
+    Serial.println(colorRight.value);
+    send(colorRight);
     slider3.prev = slider3.value;
   }
 
   if (sliderValueChanged(slider4)) {
-    Serial.print("Slider 4 changed ");
-    Serial.println(slider4.value);
-
-    // TODO
-    //send();
-
+    speed.value = sliderToSpeed(slider4.value);
+    Serial.print("SPEED changed: ");
+    Serial.println(speed.value);
+    send(speed);
     slider4.prev = slider4.value;
   }
 
@@ -161,39 +215,27 @@ void loop() {
       send(strobeOff);
     }
 
-    if (isButtonPressed(redButton)) {
-      if (!redButton.pressed) {
-        Serial.println("Red button pressed");
-        redButton.pressed = true;
-
-        // TODO
-        //send();
-      }
-    } else {
-      redButton.pressed = false;
-    }
-
     if (isButtonPressed(yellowButton)) {
       if (!yellowButton.pressed) {
-        Serial.println("Yellow button pressed");
+        Serial.println("Yellow button: TWINKLE");
         yellowButton.pressed = true;
 
-        // TODO
-        //send();
+        setBackground(VIZ_TWINKLE);
+        send(background);
+        backgroundTimer.reset();
       }
     } else if (yellowButton.pressed) {
-      //Serial.println("Strobe Off");
       yellowButton.pressed = false;
-      //send(strobeOff);
     }
 
     if (isButtonPressed(blueButton)) {
       if (!blueButton.pressed) {
-        Serial.println("Blue button pressed");
+        Serial.println("Blue button: EXPLODE");
         blueButton.pressed = true;
 
-        // TODO
-        //send();
+        setBackground(VIZ_EXPLODE);
+        send(background);
+        backgroundTimer.reset();
       }
     } else {
       blueButton.pressed = false;
@@ -201,14 +243,27 @@ void loop() {
 
     if (isButtonPressed(greenButton)) {
       if (!greenButton.pressed) {
-        Serial.println("Green button pressed");
+        Serial.println("Green button: SPIN");
         greenButton.pressed = true;
+
+        setBackground(VIZ_SPIN);
+        send(background);
+        backgroundTimer.reset();
+      }
+    } else {
+      greenButton.pressed = false;
+    }
+
+    if (isButtonPressed(redButton)) {
+      if (!redButton.pressed) {
+        Serial.println("Red button: CYCLE COLOR MODE");
+        redButton.pressed = true;
 
         // TODO
         //send();
       }
     } else {
-      greenButton.pressed = false;
+      redButton.pressed = false;
     }
   }
 }
