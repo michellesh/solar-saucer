518f547d1128ccbbbef18ff6872d23c443d16e51
Michelle Sharer <michellesharer@gmail.com>
2021-10-07 16:07:30 -0600

    Working with all 7 buttons and all 4 knobs

--- 74hc165withPots/74hc165withPots.ino
+++ 74hc165withPots/74hc165withPots.ino
@@ -22,11 +22,18 @@ int SR_CLOCK_ENABLE = D5;
 int SR_DATA = D6;
 int SR_CLOCK = D7;
 
-int KNOB_RED = D0;
-int KNOB_WHITE = D1;
+int KNOB_1 = D0;
+int KNOB_2 = D1;
+int KNOB_3 = D2;
+int KNOB_4 = D3;
 
-int BUTTON_YELLOW = SR_D4;
-int BUTTON_BLUE = SR_D5;
+int BUTTON_1 = SR_D1;
+int BUTTON_2 = SR_D2;
+int BUTTON_3 = SR_D3;
+int BUTTON_4 = SR_D4;
+int BUTTON_5 = SR_D5;
+int BUTTON_6 = SR_D6;
+int BUTTON_7 = SR_D7;
 
 void setup() {
   Serial.begin(115200);
@@ -38,8 +45,10 @@ void setup() {
   pinMode(SR_DATA, INPUT);
 
   // Potentiometers
-  pinMode(KNOB_WHITE, OUTPUT);
-  pinMode(KNOB_RED, OUTPUT);
+  pinMode(KNOB_1, OUTPUT);
+  pinMode(KNOB_2, OUTPUT);
+  pinMode(KNOB_3, OUTPUT);
+  pinMode(KNOB_4, OUTPUT);
   pinMode(A0, INPUT);
 }
 
@@ -56,43 +65,52 @@ void loop() {
   byte incoming = shiftIn(SR_DATA, SR_CLOCK, LSBFIRST);
   digitalWrite(SR_CLOCK_ENABLE, HIGH);
 
-  // Print to serial monitor
+  // Print button values
+  Serial.print("Button 1: ");
+  Serial.println(getButtonValue(incoming, BUTTON_1));
+  Serial.print("Button 2: ");
+  Serial.println(getButtonValue(incoming, BUTTON_2));
+  Serial.print("Button 3: ");
+  Serial.println(getButtonValue(incoming, BUTTON_3));
+  Serial.print("Button 4: ");
+  Serial.println(getButtonValue(incoming, BUTTON_4));
+  Serial.print("Button 5: ");
+  Serial.println(getButtonValue(incoming, BUTTON_5));
+  Serial.print("Button 6: ");
+  Serial.println(getButtonValue(incoming, BUTTON_6));
+  Serial.print("Button 7: ");
+  Serial.println(getButtonValue(incoming, BUTTON_7));
+
+  // Print knob values
+  Serial.print("Knob 1: ");
+  Serial.println(getKnobValue(KNOB_1));
+  Serial.print("Knob 2: ");
+  Serial.println(getKnobValue(KNOB_2));
+  Serial.print("Knob 3: ");
+  Serial.println(getKnobValue(KNOB_3));
+  Serial.print("Knob 4: ");
+  Serial.println(getKnobValue(KNOB_4));
+
+  Serial.println();
+  delay(500);
+}
+
+int getKnobValue(int knob) {
+  digitalWrite(knob, HIGH);
+  delay(100);
+  int knobValue = analogRead(0);
+  digitalWrite(knob, LOW);
+  return knobValue;
+}
+
+int getButtonValue(byte incoming, int button) {
+  return bitRead(incoming, button) ? 0 : 1;
+}
+
+void printByte(byte incoming) {
   for (int i = 0; i < 8; i++) {
     bool b = bitRead(incoming, i);
     Serial.print(b);
   }
   Serial.println();
-
-  Serial.print("Yellow button: ");
-  if (bitRead(incoming, BUTTON_YELLOW)) {
-    Serial.println("OFF");
-  } else {
-    Serial.println("ON");
-  }
-  Serial.print("Blue button: ");
-  if (bitRead(incoming, BUTTON_BLUE)) {
-    Serial.println("OFF");
-  } else {
-    Serial.println("ON");
-  }
-
-  // Potentiometers
-  digitalWrite(KNOB_WHITE, HIGH);
-  delay(100);
-  int knobWhiteValue = analogRead(0);
-  digitalWrite(KNOB_WHITE, LOW);
-
-  digitalWrite(KNOB_RED, HIGH);
-  delay(100);
-  int knobRedValue = analogRead(0);
-  digitalWrite(KNOB_RED, LOW);
-
-  // Print pot values
-  Serial.print("White knob: ");
-  Serial.println(knobWhiteValue);
-  Serial.print("Red knob: ");
-  Serial.println(knobRedValue);
-
-  Serial.println();
-  delay(500);
 }
